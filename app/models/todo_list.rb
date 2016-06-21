c75579dc1fce92c7e34e0137f492550de5298283
Nolan Frazier <nofrazier3@gmail.com>
2016-06-20 20:17:47 -0700

    Added validations for description on todo_list

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,7 @@
 class TodoList < ApplicationRecord
   validates :title, presence: true,
                     length: { minimum: 3 }
+
+  validates :description, presence: true,
+                          length: { minimum: 5 }
 end
